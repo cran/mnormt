57ea383ac89ac8a031f642a3157200d2b8e6c951
Adelchi Azzalini <azzalini@stat.unipd.it>
2006-10-26 15:47:14 +0000

    version 1.2-0

--- R/mnormt.R
+++ R/mnormt.R
@@ -84,7 +84,7 @@ rmt <- function(n=1, mean=rep(0,d), S, df=Inf)
   d <- if(is.matrix(S)) ncol(S) else 1 
   if(df==Inf) x <-1 else x <- rchisq(n,df)/df
   z <- rmnorm(n, rep(0,d), S)
-  y <- t(mean + t(sqrt(x)*z))
+  y <- t(mean + t(z/sqrt(x)))
   return(y)
 }
 
